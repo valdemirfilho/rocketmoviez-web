90ccc69761c3d761d1b0171fcf87219e0dd52269
Valdemir Filho <filhux@gmail.com>
2023-03-29 16:49:40 -0300

    test: railway

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ import axios from "axios"
 export const api = axios.create({
   baseURL: import.meta.env.DEV
     ? "http://localhost:5000"
-    : "https://rocketmoviez-api.fly.dev"
+    : "https://rocketmoviez-api-production.up.railway.app"
+  // : "https://rocketmoviez-api.fly.dev"
 })
 
