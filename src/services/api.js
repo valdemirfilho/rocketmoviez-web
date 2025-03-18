f4ae2d6914ec68911be34735f86814e8cae5e6a1
Valdemir Filho <filhux@gmail.com>
2025-03-18 11:41:37 -0300

    fix backend

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export const api = axios.create({
   baseURL: import.meta.env.DEV
     ? "http://localhost:5000"
-    : "https://rocketmoviez-api.fly.dev"
+    : "https://rocketmoviez-api.onrender.com"
+  // : "https://rocketmoviez-api.fly.dev"
   // : "https://rocketmoviez-api-production.up.railway.app"
 })
-
